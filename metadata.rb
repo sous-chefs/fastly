42faa96adf1a9f9aee90c15b7c09c525bb624a7f
Christopher Webber <cwebberops@gmail.com>
2015-04-07 22:11:13 -0700

    bump the version

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'cia@chef.io'
 license          'Apache 2.0'
 description      'Resources for Configuring Fastly Services'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.0.2'
