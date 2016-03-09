8b209333f83f94c53f2cab2075503b3f6fe4ace2
Christopher Webber <cwebber@chef.io>
2016-03-09 13:48:37 -0800

    Pin gem version
    This ensures we have the correct version of the gem installed.

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'cia@chef.io'
 license          'Apache 2.0'
 description      'Resources for Configuring Fastly Services'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.16'
+version          '0.0.17'
 source_url       'https://github.com/chef-cookbooks/fastly'
