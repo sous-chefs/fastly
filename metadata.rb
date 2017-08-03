67334ab2c26c2ddcaf350e77f31a97c2638cb499
Christopher Webber <cwebber@chef.io>
2017-08-02 17:39:54 -0700

    Bump version
    Signed-off-by: Christopher Webber <cwebber@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cia@chef.io'
 license          'Apache-2.0'
 description      'Resources for Configuring Fastly Services'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.0'
+version          '0.3.1'
 source_url       'https://github.com/chef-cookbooks/fastly'
 
 gem 'fastly'
