b23c41bd7c37a8b455f0070cbd58c3656337e693
Lance Albertson <lance@osuosl.org>
2021-03-22 13:22:18 -0700

    Restrict to Chef < 16
    This is still using the old style provider/resources and does not work on >= 16.
    We should get this converted later.
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,7 @@ description       'Resources for Configuring Fastly Services'
 version           '0.5.0'
 source_url        'https://github.com/sous-chefs/fastly'
 issues_url        'https://github.com/sous-chefs/fastly/issues'
-chef_version      '>= 12.10'
+# TODO(ramereth): This is still using the old style provider/resources and does not work on >= 16
+chef_version      '>= 12.10', '< 16.0'
 
 supports 'any'
